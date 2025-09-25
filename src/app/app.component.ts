ca97fed373c4a02585173a60e1c59d3b66e29bb4
VitorReis <vitor_hvr@outlook.com>
2025-09-25 12:08:24 -0300

    autenticação html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { ListaUsuariosComponent } from "./lista-usuarios/lista-usuarios.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListaUsuariosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
 
+  form = this.fb.group({
+    email: ['', [Validators.required]],
+    senha: ['', [Validators.required]]
+  });
+
+  async onSubmit(): Promise<void>{
+    if(this.form.invalid) return;
+
+    try{
+      const {email, senha} = this.form.getRawValue();
+
+      if(email == null || senha == null){
+        alert("Email e senha são obrigatórios.");
+        return;
+      }
+
+      var token = await this.auth.login({email, senha});
+      alert(token);
+    } catch(e: any){
+      alert(e.error.message);
+    }
+  }
 }
