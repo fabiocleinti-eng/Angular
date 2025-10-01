4f6e3564e74d2217f70b3b604bbabb129db05289
VHREIS <VHREIS@SES505197>
2025-09-30 23:07:12 -0300

    Guard e Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth.guard';
+import { LoginComponent } from './login/login.component';
 import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 import { FormularioComponent } from './formulario/formulario.component';
 
 
 export const routes: Routes = [
-    { path: 'usuarios', component: ListaUsuariosComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'login', component: LoginComponent },
     { path: 'formulario', component: FormularioComponent },
+    
     {
-        path: '', /* raiz do projeto */
-        pathMatch: 'full',
-        redirectTo: 'formulario'
-    }
+    path: 'lista-usuarios',
+    component: ListaUsuariosComponent,
+    canActivate: [authGuard],
+    },
+
+    { path: '**', redirectTo: 'login' },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router'; 
+import { AuthService } from '../services/auth.service';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService)
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
+  loading = signal(false);
+  error = signal<string | null>(null);
+
+  form = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    senha: ['', [Validators.required]]
+  });
+
+  async onSubmit() {
+    if (this.form.invalid) {
+      return;
+    } 
+
+    this.loading.set(true);
+    this.error.set(null);
+
+    try{
+      const { email, senha } = this.form.getRawValue();
+      if(email == null || senha == null) {
+        this.error.set('Email e Senha são obrigatórios');
+        this.loading.set(false);
+        return;
+      }
+      var token = await this.authService.login({email, senha});
+      this.router.navigate(['/lista-usuarios']);
+    }catch(err:any){
+      this.error.set(err.message || 'Erro ao realizar login');
+    }finally{
+      this.loading.set(false);
+    }
+  } 
 }
